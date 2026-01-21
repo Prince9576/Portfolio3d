c143c03d156dd5c9a7494e6eb7544fb7e2f5cf81
Prince Kumar <princekumar@Princes-MacBook-Air.local>
2026-01-21 22:46:11 +0530

    Ipad sumport

--- src/utils/mobileDetection.js
+++ src/utils/mobileDetection.js
@@ -1,7 +1,7 @@
 // Mobile detection utility
 export const isMobile = () => {
-  return typeof window !== 'undefined' && window.innerWidth < 768;
+  return typeof window !== 'undefined' && window.innerWidth < 1028;
 };
 
 // Mobile breakpoint constant
-export const MOBILE_BREAKPOINT = 768;
+export const MOBILE_BREAKPOINT = 1024;
